ef3aa9116237ff4f28aff49f2f06b112cb1fa7d1
Mimo604 <78756390+Mimo604@users.noreply.github.com>
2022-01-13 18:58:55 -0500

    Skeleton for Pneumatics done

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -12,4 +12,10 @@ package frc.robot;
  * <p>It is advised to statically import this class (or one of its inner classes) wherever the
  * constants are needed, to reduce verbosity.
  */
-public final class Constants {}
+public final class Constants {
+    public static class kPneumatics {
+        public static final double MIN_PSI = 0;
+        public static final double MAX_PSI = 60;
+
+    }
+}
