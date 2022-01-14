95c9b77063921d53e1a58e08e707043017e6211e
Mkath1423 <diamondcrafter1423@gmail.com>
2022-01-14 16:47:01 -0500

    Fixed auto command
    Made auto drive backwards instead of forwards.

--- src/main/java/frc/robot/commands/DriveStraight.java
+++ src/main/java/frc/robot/commands/DriveStraight.java
@@ -28,7 +28,7 @@ public class DriveStraight extends CommandBase {
     // Called every time the scheduler runs while the command is scheduled.
     @Override
     public void execute() {
-        sys_drive.tankDrive(-driveSpeed, -driveSpeed);
+        sys_drive.tankDrive(driveSpeed, driveSpeed);
     }
   
     // Called once the command ends or is interrupted.
