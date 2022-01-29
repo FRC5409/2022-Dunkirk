2dfd5210eaefef447c7136eaedac15699cfc01ce
Mochimochimochimochimochi <elizabethcaporiccio@gmail.com>
2022-01-29 10:05:35 -0500

    Command that runs both motors for indexer testing

--- src/main/java/frc/robot/commands/TestIndexProto.java
+++ src/main/java/frc/robot/commands/TestIndexProto.java
@@ -1,7 +1,32 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.IntakeIndexer;
 
 public class TestIndexProto extends CommandBase{
+    @SuppressWarnings({"PMD.UnusedPrivateField", "PMD.SingularField"})
+    private IntakeIndexer sys_intakeIndexer;
+
+    public TestIndexProto(IntakeIndexer subsystem){
+        sys_intakeIndexer = subsystem; 
+
+        addRequirements(subsystem);
+    }
+
+    @Override
+    public void initialize(){}
+
+    @Override
+    public void execute(){
+        sys_intakeIndexer.indexShootOn(1);
+        sys_intakeIndexer.indexBeltOn(1);
+    }
+
+    @Override
+    public void end(boolean interuppted){}
+
+    @Override
+    public boolean isFinished(){return false;}
+    
     
 }
