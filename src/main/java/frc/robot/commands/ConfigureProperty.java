1d2bc1e4bd8a9cb14202c6585902f21eb686085c
Keith Davies <32884707+keith-davies@users.noreply.github.com>
2022-03-02 16:13:37 -0500

    add consumer and supplier support to configure property

--- src/main/java/frc/robot/commands/ConfigureProperty.java
+++ src/main/java/frc/robot/commands/ConfigureProperty.java
@@ -1,21 +1,32 @@
 package frc.robot.commands;
 
+import java.util.function.Consumer;
+import java.util.function.Supplier;
+
 import edu.wpi.first.wpilibj2.command.CommandBase;
 
 import frc.robot.base.Property;
 
 public class ConfigureProperty<T> extends CommandBase {
+    private final Consumer<Property<T>> consumer;
     private final Property<T> property;
-    private final T           value;
+    
+    public ConfigureProperty(Property<T> property, Consumer<Property<T>> consumer) {
+        this.property = property;
+        this.consumer = consumer;
+    }
+
+    public ConfigureProperty(Property<T> property, Supplier<T> supplier) {
+        this(property, p -> p.set(supplier.get()));
+    }
 
     public ConfigureProperty(Property<T> property, T value) {
-        this.property = property;
-        this.value    = value;
+        this(property, p -> p.set(value));
     }
 
     @Override
     public void initialize() {
-        property.set(value);
+        consumer.accept(property);
     }
 
     @Override
@@ -23,4 +34,7 @@ public class ConfigureProperty<T> extends CommandBase {
         return true;
     }
     
+    public Property<T> getProperty() {
+        return property;
+    }
 }
