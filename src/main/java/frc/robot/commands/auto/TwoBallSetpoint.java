903d6fe2cd0937a7ce8650f3c4296147cc09ccfb
Lex Stapleton <diamondcrafter1423@gmail.com>
2022-03-02 10:59:44 -0500

    Added shooting into auto routines

--- src/main/java/frc/robot/commands/auto/TwoBallSetpoint.java
+++ src/main/java/frc/robot/commands/auto/TwoBallSetpoint.java
@@ -1,34 +1,61 @@
 package frc.robot.commands.auto;
 
+
+import frc.robot.Constants;
+import frc.robot.base.ValueProperty;
+import frc.robot.base.shooter.ShooterConfiguration;
+import frc.robot.base.shooter.ShooterMode;
+import frc.robot.base.shooter.ShooterModel;
+import frc.robot.base.shooter.SweepDirection;
+
+// commands
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import edu.wpi.first.wpilibj2.command.ParallelRaceGroup;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+
 import frc.robot.commands.IndexerIntakeActive;
 import frc.robot.commands.MoveToDistance;
+import frc.robot.commands.shooter.OperateShooter;
+import frc.robot.commands.shooter.RotateTurret;
+// subsystems
+import frc.robot.subsystems.shooter.ShooterFlywheel;
+import frc.robot.subsystems.Limelight;
 import frc.robot.subsystems.DriveTrain;
 import frc.robot.subsystems.Indexer;
 import frc.robot.subsystems.Intake;
+import frc.robot.subsystems.shooter.ShooterTurret;
 
 public class TwoBallSetpoint extends SequentialCommandGroup {
 
     private final DriveTrain driveTrain;
     private final Intake intake;
     private final Indexer indexer;
-    // private final Shooter?
+    private final ShooterFlywheel flywheel;
+    private final ShooterTurret turret;
+    private final Limelight limelight;
+    
 
-    public TwoBallSetpoint(DriveTrain driveTrain, Intake intake, Indexer indexer) {
+    public TwoBallSetpoint(DriveTrain driveTrain, Intake intake, Indexer indexer, Limelight limelight, ShooterTurret turret, ShooterFlywheel flywheel) {
         
         this.driveTrain = driveTrain;
         this.intake     = intake;
         this.indexer    = indexer;
+        this.flywheel   = flywheel;
+        this.turret     = turret;
+        this.limelight  = limelight;
+
+        ValueProperty<ShooterConfiguration> shooterConfiguration = new ValueProperty<ShooterConfiguration>(Constants.Shooter.CONFIGURATIONS.get(ShooterMode.kFar));
+        ValueProperty<SweepDirection> shooterSweepDirection = new ValueProperty<SweepDirection>(SweepDirection.kLeft);
 
         addCommands(
             new ParallelRaceGroup(
                 new MoveToDistance(driveTrain, -10f),
-                new IndexerIntakeActive(indexer, intake)
+                new IndexerIntakeActive(indexer, intake),
+
+                new OperateShooter(limelight, turret, flywheel, indexer, shooterSweepDirection, shooterConfiguration),
+                new RotateTurret(turret, 0)
             )
-            //Shoot
 
         );
 
