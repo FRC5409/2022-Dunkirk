9a7eb67accf743eb32956a25e9767f66fcc6a099
Keith Davies <32884707+keith-davies@users.noreply.github.com>
2022-03-27 13:14:41 -0400

    refactor functions to return filter factory instead of instance

--- src/main/java/frc/robot/base/shooter/target/TargetFiltering.java
+++ src/main/java/frc/robot/base/shooter/target/TargetFiltering.java
@@ -3,7 +3,15 @@ package frc.robot.base.shooter.target;
 import java.util.function.Supplier;
 
 public final class TargetFiltering {
-    public static HighPassFilter highPass(Supplier<Double> kSmoothingFactor) {
-        return new HighPassFilter(kSmoothingFactor);
+    public static FilterFactory<HighPassFilter> highPass(double kSmoothingFactor) {
+        return () -> new HighPassFilter(() -> kSmoothingFactor);
+    }
+
+    public static FilterFactory<HighPassFilter> highPass(Supplier<Double> kSmoothingFactor) {
+        return () -> new HighPassFilter(kSmoothingFactor);
+    }
+
+    public static FilterFactory<FilterBase> none() {
+        return NullFilter::new;
     }
 }
