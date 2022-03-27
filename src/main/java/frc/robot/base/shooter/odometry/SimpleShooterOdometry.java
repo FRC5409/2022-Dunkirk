8c22c23cc9f5aa6bc61bf88a2913227907072bfc
Keith Davies <32884707+keith-davies@users.noreply.github.com>
2022-03-27 12:05:26 -0400

    Integrate acquisition timeout and target filtering into simple odometry

--- src/main/java/frc/robot/base/shooter/odometry/SimpleShooterOdometry.java
+++ src/main/java/frc/robot/base/shooter/odometry/SimpleShooterOdometry.java
@@ -1,6 +1,10 @@
 package frc.robot.base.shooter.odometry;
 
+import org.jetbrains.annotations.Nullable;
+
 import edu.wpi.first.util.sendable.SendableBuilder;
+import edu.wpi.first.wpilibj.Timer;
+import frc.robot.base.shooter.target.FilterBase;
 import frc.robot.utils.Vector2;
 import frc.robot.utils.Vector3;
 
@@ -8,14 +12,21 @@ import frc.robot.utils.Vector3;
  * Experimental shooter position relative odometry.
  */
 public class SimpleShooterOdometry extends OdometryBase {
+    protected final FilterBase filter;
+
     protected double kLastDistance;
     protected Vector2 kLastTarget;
+    protected double kLastUpdate;
 
-    public SimpleShooterOdometry(ShooterOdometryModel model) {
+
+    public SimpleShooterOdometry(ShooterOdometryModel model, FilterBase filter) {
         super(model);
         
         kLastDistance = 0;
-        kLastTarget = new Vector2();
+        kLastTarget = null;
+        kLastUpdate = 0;
+
+        this.filter = filter;
     }
 
     /**
@@ -23,32 +34,43 @@ public class SimpleShooterOdometry extends OdometryBase {
      * @param target   The observed vision target
      */
     public void update(Vector2 target) {
-        Vector3 observerVector = calculateTargetProjection(target);
+        Vector3 observerVector = calculateTargetProjection(filter.update(target));
 
         kLastDistance = model.kHeight / Math.tan(Math.asin(observerVector.z));
         if (!Double.isFinite(kLastDistance))
             kLastDistance = 0;
 
         kLastTarget = new Vector2(target);
+        kLastUpdate = Timer.getFPGATimestamp();
     }
 
     @Override
     public void reset() {
         kLastDistance = 0;
-        kLastTarget = new Vector2();
+        kLastTarget = null;
+        kLastUpdate = Timer.getFPGATimestamp();
     }
 
     public double getDistance() {
         return kLastDistance;
     }
 
+    @Nullable
     public Vector2 getTarget() {
         return new Vector2(kLastTarget);
     }
 
+    public boolean hasTarget(){
+        return kLastTarget != null;
+    }
+
+    public boolean isLost() {
+        return (Timer.getFPGATimestamp() - kLastUpdate) > model.kkAcquistionTimeout;
+    }
+
     @Override
     public void initSendable(SendableBuilder builder) {
-        builder.addStringProperty("Target", () -> getTarget().toString(), null);
+        builder.addStringProperty("Target", () -> String.valueOf(getTarget()), null);
         builder.addDoubleProperty("Distance", this::getDistance, null);
     }
 }
