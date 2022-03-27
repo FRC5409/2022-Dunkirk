dd54230dd2c6980c545077f6bcecba4b58e8ddb4
Keith Davies <32884707+keith-davies@users.noreply.github.com>
2022-03-27 12:31:32 -0400

    Add property casting

--- src/main/java/frc/robot/base/Property.java
+++ src/main/java/frc/robot/base/Property.java
@@ -7,6 +7,15 @@ public interface Property<T> {
     public static <T> Property<T> of(Consumer<T> setter, Supplier<T> getter) {
         return new SimpleProperty<>(setter, getter);
     }
+
+    @SuppressWarnings("unchecked")
+    public static <T, V> Property<V> cast(Property<T> property) {
+        return new SimpleProperty<>(
+             x -> property.set((T) x),
+            () -> { return (V) property.get(); }
+        );
+    }
+
     T set(T value);
 
     T get();
