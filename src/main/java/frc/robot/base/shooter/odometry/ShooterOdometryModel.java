d7fcaa6df94139712be40c6e3dea626371fd934d
Keith Davies <32884707+keith-davies@users.noreply.github.com>
2022-03-27 12:04:23 -0400

    add acquisition timeout to shooter model

--- src/main/java/frc/robot/base/shooter/odometry/ShooterOdometryModel.java
+++ src/main/java/frc/robot/base/shooter/odometry/ShooterOdometryModel.java
@@ -8,17 +8,20 @@ public class ShooterOdometryModel implements Sendable {
     public final double kPitch;
     public final double kHeight;
     public final double kOffset;
+    public final double kkAcquistionTimeout;
     public final Vector2 kFieldOfView;
     
     public ShooterOdometryModel(
         double kPitch,
         double kHeight,
         double kOffset,
+        double kAcquistionTimeout,
         Vector2 kFieldOfView
     ) {
         this.kPitch = kPitch;
         this.kHeight = kHeight;
         this.kOffset = kOffset;
+        this.kkAcquistionTimeout = kAcquistionTimeout;
         this.kFieldOfView = kFieldOfView;
     }
 
@@ -27,6 +30,7 @@ public class ShooterOdometryModel implements Sendable {
         builder.addDoubleProperty("kPitch", () -> kPitch, null); 
         builder.addDoubleProperty("kHeight", () -> kHeight, null); 
         builder.addDoubleProperty("kOffset", () -> kOffset, null); 
+        builder.addDoubleProperty("kkAcquistionTimeout", () -> kkAcquistionTimeout, null); 
         builder.addStringProperty("kFieldOfView", () -> kFieldOfView.toString(), null);
     }
 }
