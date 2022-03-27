ba0281a92e492d2399c97788b422db57b158477a
Duriiam <sanad@alajrami.com>
2022-03-27 10:27:02 -0400

    auto update

--- src/main/java/frc/robot/commands/autonomous/trajectory/trajectoryAuto/TwoBallsAuto.java
+++ src/main/java/frc/robot/commands/autonomous/trajectory/trajectoryAuto/TwoBallsAuto.java
@@ -68,7 +68,7 @@ public class TwoBallsAuto extends SequentialCommandGroup{
 
         Trajectory t1 = TrajectoryGenerator.generateTrajectory(new Pose2d(0, 0, new Rotation2d(0)),
                                                                    List.of(),
-                                                                   new Pose2d(1.75/kAuto.kDistanceRatio, 0, new Rotation2d(0)),
+                                                                   new Pose2d(1.5/kAuto.kDistanceRatio, 0, new Rotation2d(0)),
                                                                    kAuto.configForwards);
 
         RamseteCommand r1 = new RamseteCommand(t1, m_drive::getPose,
