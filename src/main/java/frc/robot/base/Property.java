8a2f9797cdbc1006df4caa2f5d6cba5b3fbb82d8
Keith Davies <32884707+keith-davies@users.noreply.github.com>
2022-03-27 12:31:11 -0400

    add SimpleProperty, and ValueProperty of()

--- src/main/java/frc/robot/base/Property.java
+++ src/main/java/frc/robot/base/Property.java
@@ -1,6 +1,12 @@
 package frc.robot.base;
 
+import java.util.function.Consumer;
+import java.util.function.Supplier;
+
 public interface Property<T> {
+    public static <T> Property<T> of(Consumer<T> setter, Supplier<T> getter) {
+        return new SimpleProperty<>(setter, getter);
+    }
     T set(T value);
 
     T get();
