29afa127b89e164c2056b451f67983e4f5c9cc25
Keith Davies <32884707+keith-davies@users.noreply.github.com>
2022-04-11 18:39:08 -0400

    add consumers and suppliers to CommandProperty methods

--- src/main/java/frc/robot/base/CommandProperty.java
+++ src/main/java/frc/robot/base/CommandProperty.java
@@ -1,5 +1,8 @@
 package frc.robot.base;
 
+import java.util.function.Consumer;
+import java.util.function.Supplier;
+
 import frc.robot.base.command.WaitConditionCommand;
 import frc.robot.commands.ConfigureProperty;
 
@@ -15,12 +18,28 @@ public class CommandProperty<T> extends ValueProperty<T> {
     public ConfigureProperty<T> configureTo(T targetValue) {
         return new ConfigureProperty<>(this, targetValue);
     }
+    
+    public ConfigureProperty<T> configureTo(Supplier<T> supplier) {
+        return new ConfigureProperty<>(this, supplier);
+    }
+    
+    public ConfigureProperty<T> configureTo(Consumer<Property<T>> consumer) {
+        return new ConfigureProperty<>(this, consumer);
+    }
 
     public WaitConditionCommand equalTo(T expectedValue) {
-        return new WaitConditionCommand(() -> m_value == expectedValue);
+        return new WaitConditionCommand(() -> m_value.equals(expectedValue));
+    }
+
+    public WaitConditionCommand equalTo(Supplier<T> expectedValue) {
+        return new WaitConditionCommand(() -> m_value.equals(expectedValue.get()));
     }
 
     public WaitConditionCommand notEqualTo(T expectedValue) {
-        return new WaitConditionCommand(() -> m_value != expectedValue);
+        return new WaitConditionCommand(() -> !m_value.equals(expectedValue));
+    }
+
+    public WaitConditionCommand notEqualTo(Supplier<T> expectedValue) {
+        return new WaitConditionCommand(() -> !m_value.equals(expectedValue.get()));
     }
 }
