881543f1e6ab4198dbe2cc78f275fae38af344c8
Keith Davies <32884707+keith-davies@users.noreply.github.com>
2022-04-11 18:40:32 -0400

    add missing requirements

--- src/main/java/frc/robot/commands/shooter/state/operate/RunShooterState.java
+++ src/main/java/frc/robot/commands/shooter/state/operate/RunShooterState.java
@@ -60,6 +60,8 @@ public class RunShooterState extends StateBase {
         this.driveSpeed = driveSpeed;
         this.flywheel = flywheel;
         this.indexer = indexer;
+
+        addRequirements(flywheel, indexer);
     }
 
     @Override
